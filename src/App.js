3c51f4f13dea140c705d013d5d00bcc560692d94
Sharif Mohammad Eunus <sharif.eunus@gmail.com>
2022-05-09 19:06:03 +0600

    label added

--- src/App.js
+++ src/App.js
@@ -6,36 +6,74 @@ function App() {
 	const [target, setTarget] = useState('');
 	return (
 		<div className="App container">
-			<input
-				className="input"
-				type="text"
-				name="source"
-				id="source"
-				value={source}
-				onChange={(e) => setSouce(e.target.value)}
-			/>
-			<br />
-			<input
-				className="input"
-				type="text"
-				name="destination"
-				id="destination"
-				value={target}
-				onChange={(e) => setTarget(e.target.value)}
-			/>
-			<code>
-				wp search-replace {source} {target} --skip-themes --skip-plugins
+			<div className="field">
+				<label className="label" htmlFor="source">
+					Source
+				</label>
+				<div className="control">
+					<input
+						className="input"
+						type="text"
+						name="source"
+						id="source"
+						value={source}
+						onChange={(e) => setSouce(e.target.value)}
+					/>
+				</div>
+			</div>
+			<div className="field">
+				<label className="label" htmlFor="destination">
+					Destination
+				</label>
+				<div className="control">
+					<input
+						className="input"
+						type="text"
+						name="destination"
+						id="destination"
+						value={target}
+						onChange={(e) => setTarget(e.target.value)}
+					/>
+				</div>
+			</div>
+
+			<code className="card">
+				<span>
+					wp search-replace {source} {target} --skip-themes --skip-plugins
+				</span>
+				<button className="button" aria-label="Copy to clipboard">
+					📋
+				</button>
 			</code>
-			<code>
-				{' '}
-				wp search-replace http://{target} https://{target} --skip-themes
-				--skip-plugins
+			<code className="card">
+				<span>
+					wp search-replace http://{target} https://{target} --skip-themes
+					--skip-plugins
+				</span>
+
+				<button className="button" aria-label="Copy to clipboard">
+					📋
+				</button>
 			</code>
-			<code>
+			<code className="card">
 				wp search-replace @{target} @{source} --skip-themes --skip-plugins
+				<span></span>
+				<button className="button" aria-label="Copy to clipboard">
+					📋
+				</button>
 			</code>
-			<code>wp option update upload_path ''</code>
-			<code>wp cache flush</code>
+			<code className="card">
+				<span>wp option update upload_path ''</span>
+				<button className="button" aria-label="Copy to clipboard">
+					📋
+				</button>
+			</code>
+			<code className="card">
+				<span>wp cache flush</span>
+				<button className="button" aria-label="Copy to clipboard">
+					📋
+				</button>
+			</code>
 		</div>
 	);
 }
