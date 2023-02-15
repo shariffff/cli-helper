d036311539c761ba154272e49f8c18f1374c1ee9
Sharif Mohammad Eunus <sharif.eunus@gmail.com>
2023-02-15 14:55:02 +0600

    keyboard accessibility support

--- src/App.js
+++ src/App.js
@@ -72,7 +72,7 @@ function App() {
 			<hr/>
 			<h6 style={{textAlign: 'center'}}>All</h6>
 			<hr/>
-			{ !network ? (<code className="card">
+			{ !network ? (<code className="card" contentEditable>
 				wp search-replace {source} {target} --skip-themes --skip-plugins &&
 				wp search-replace http://{target} https://{target} --skip-themes --skip-plugins &&
 				wp search-replace @{target} @{  source.startsWith('www.') ? source.replace('www.', '') : source } --skip-themes --skip-plugins && wp option update upload_path '' --skip-themes --skip-plugins && wp hosting clearcache all --skip-plugins --skip-themes
